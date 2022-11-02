6379fbbb1d391daec69bc336fe59c0c9d4d34e7e
TomaszPuchala <tomaszpuchala@interia.pl>
2022-11-02 21:07:06 +0100

    02.11 update

---
+++ AD/reset_passwords_multiple.ps1
@@ -0,0 +1,24 @@
+﻿Import-module ActiveDirectory
+ $Users = Get-Content $PSScriptRoot\users.txt
+ foreach ($User in $Users) {
+     If(Get-ADUser -Filter "SamAccountName -eq '$user'")
+     {
+     Unlock-ADAccount –Identity $user
+     $Password = Read-Host "Enter new password for user "$user"" 
+     $Pass = ConvertTo-SecureString $Password -AsPlainText -For
+     Set-ADAccountPassword $user -NewPassword $Pass -Reset
+     #Force a password change after LogOn
+     Set-ADUser -Identity $user -ChangePasswordAtLogon $true
+     Write-Host $user, $Password
+             [pscustomobject]@{
+        User     = $user
+        Password = $Password
+    } | 
+    export-csv "passwords_multiple.csv" -NoTypeInformation -Append
+     }
+     else
+     {
+     "User $user doesn't exist"
+     }
+     }
+
